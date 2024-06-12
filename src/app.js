1759eac2f09ea7b1be6cd6f970ec156489c9abb2
Miranda <77069174+MagadeOz@users.noreply.github.com>
2024-06-12 01:13:55 +0000

    Primer commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //variables
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  //funciones
+  function generadorDeAleatorio(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  function generadorDeExcusa() {
+    const randomWho = generadorDeAleatorio(who);
+    const randomAction = generadorDeAleatorio(action);
+    const randomWhat = generadorDeAleatorio(what);
+    const randomWhen = generadorDeAleatorio(when);
+    return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+  }
+
+  document.getElementById("excuse").innerHTML = generadorDeExcusa();
 };
